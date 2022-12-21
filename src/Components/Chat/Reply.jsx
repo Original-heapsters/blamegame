6b6d79d943b656ec6669e9e987b5f45feb5f6980
rotciv03 <vctrsls3@gmail.com>
2022-12-20 19:13:47 -0800

    first iteration of socket handler fixes #35

---
+++ src/Components/Chat/Reply.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import styles from './reply.module.css';
+
+export default function Reply({socket}) {
+  console.log(socket);
+  const [msg, setMsg] = useState('hello');
+  const sendMessage = () => {
+    socket.emit('chatMessage', { game: 'general', user: 'testUser', msg });
+  };
+  return ( 
+    <div className={styles.replyContainer}>
+      <div className={styles.replyCont}>
+        <input type="text" name="reply" className={styles.reply} placeholder="Reply..." onChange={(e) => { setMsg(e.target.value); }} />
+      </div>
+      <button type="button" className={styles.btn} onClick={sendMessage}>submit</button>
+    </div>
+  );
+}
