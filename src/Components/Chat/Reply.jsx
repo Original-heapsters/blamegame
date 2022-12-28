2c22764c192ed8da370bd333df1fae316fcab736
Bill Grundler <bill@davesimmons.io>
2022-12-27 20:31:49 -0800

    chat reply clears ..in-ine functions

--- src/Components/Chat/Reply.jsx
+++ src/Components/Chat/Reply.jsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import styles from './reply.module.css';
 
 export default function Reply({ socket }) {
-  const [msg, setMsg] = useState('hello');
+  const [msg, setMsg] = useState('');
+
   const sendMessage = () => {
     socket.emit('chatMessage', { game: 'general', user: 'testUser', msg });
   };
   return (
     <div className={styles.replyContainer}>
       <div className={styles.replyCont}>
-        <input type="text" name="reply" className={styles.reply} placeholder="Reply..." onChange={(e) => { setMsg(e.target.value); }} />
+        <input type="text" name="reply" className={styles.reply} placeholder="reply..." onChange={(e) => { setMsg(e.target.value); }} value={msg} />
       </div>
-      <button type="button" className={styles.btn} onClick={sendMessage}>submit</button>
+      <button type="button" className={styles.btn} onClick={() => { sendMessage(); setMsg(''); }}>submit</button>
     </div>
   );
 }
